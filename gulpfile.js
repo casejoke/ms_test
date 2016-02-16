2382cbaa6249c00c4f05669a6943e3591fa5e1b0
casejoke <casejoke@gmail.com>
2016-02-16 09:36:36 +0300

    edit gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,16 @@ var path = {
         sprite:  'www/assets/images/sprite/',
         spriteScss: 'src/style/' 
     },
+    prodaction: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html:    'www/',
+        js:      '../catalog/view/theme/' + name + '/assets/js/',
+        css:     '../catalog/view/theme/' + name + '/assets/css/',
+        images:  '../catalog/view/theme/' + name + '/assets/css/images/',
+        fonts:   '../catalog/view/theme/' + name + '/assets/fonts/',
+        fontBs:  '../catalog/view/theme/' + name + '/assets/fonts/',
+        sprite:  '../catalog/view/theme/' + name + '/assets/images/sprite/',
+        spriteScss: 'src/style/' 
+    },
     src: { //Пути откуда брать исходники
         html:    'src/html/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
         js:  [     
@@ -57,15 +67,14 @@ var path = {
                  'bower_components/jquery-ui/jquery-ui.js', 
                  'bower_components/jquery.transit/jquery.transit.js', 
         
-                  'src/js/partials/hoverable_and_touchable.js',
+                 'src/js/partials/hoverable_and_touchable.js',
                  'src/js/main.js',
 
-                'src/js/canvasTriangles.js',
+                 'src/js/canvasTriangles.js',
                  'src/js/partials/_visual.js',
                  'src/js/partials/help.js',
                  'src/js/partials/test3.js',
                  'src/js/partials/_common.js',
-                // 'src/js/partials/jquery.transit.min.js'
 
               ]   ,//В стилях и скриптах нам понадобятся только main файлы
         style:   'src/style/styles.scss',
@@ -106,8 +115,8 @@ gulp.task('sprites', function() {
                 cssName: 'sprite.scss',
             }))
 
-    spriteData.css.pipe(gulp.dest(path.build.spriteScss))
-    spriteData.img.pipe(gulp.dest(path.build.sprite))
+    spriteData.css.pipe(gulp.dest(path.prodaction.spriteScss))
+    spriteData.img.pipe(gulp.dest(path.prodaction.sprite))
     .pipe(notify({
             title: 'sprite',
             message: 'sprite Complide'
@@ -121,7 +130,7 @@ gulp.task('sprites', function() {
 gulp.task('html:build', function () {
     gulp.src(path.src.html) //Выберем файлы по нужному пути
         // .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build  
+        .pipe(gulp.dest(path.prodaction.html)) //Выплюнем их в папку build  
         .pipe(notify({
             title: 'html',
             message: 'html build Complide'
@@ -134,11 +143,11 @@ gulp.task('html:build', function () {
 gulp.task('js:build', function () {
     gulp.src(path.src.js) //Найдем наш main файл
         .pipe(concat(name+'.js'))
-        .pipe(gulp.dest(path.build.js))
+        .pipe(gulp.dest(path.prodaction.js))
         .pipe(stripDebug())
         .pipe(uglify())
         .pipe(rename(name+".min.js"))
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(gulp.dest(path.prodaction.js)) //Выплюнем готовый файл в build
         .pipe(notify({
             title: 'js',
             message: 'js build Complide'
@@ -153,11 +162,11 @@ gulp.task('style:build', function () {
         .pipe(sass().on('error', sass.logError))//Скомпилируем
         .pipe(prefixer()) //Добавим вендорные префиксы
         .pipe(rename(name+'.css'))
-        .pipe(gulp.dest(path.build.css))
+        .pipe(gulp.dest(path.prodaction.css))
         .pipe(cssmin()) //Сожмем
         .pipe(sourcemaps.write())
         .pipe(rename(name+'.min.css'))
-        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(gulp.dest(path.prodaction.css)) //И в build
         .pipe(notify({
             title: 'sass',
             message: 'sass build Complide'
@@ -174,7 +183,7 @@ gulp.task('image:build', function () {
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.images)) //И бросим в build
+        .pipe(gulp.dest(path.prodaction.images)) //И бросим в build
          .pipe(notify({
             title: 'images',
             message: 'images compress Complide'
@@ -185,7 +194,7 @@ gulp.task('image:build', function () {
 
 gulp.task('fonts:build', function() {
     gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+        .pipe(gulp.dest(path.prodaction.fonts))
          .pipe(notify({
             title: 'fonts',
             message: 'fonts copy Complide'
@@ -195,7 +204,7 @@ gulp.task('fonts:build', function() {
 
 gulp.task('fontsbs:build', function() {
     gulp.src([path.src.fontsBs, path.src.fontsAwesome])
-        .pipe(gulp.dest(path.build.fontBs))
+        .pipe(gulp.dest(path.prodaction.fontBs))
          .pipe(notify({
             title: 'fonts bs',
             message: 'fonts bs copy Complide'
